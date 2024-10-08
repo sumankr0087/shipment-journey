eb69d91ee3616ce2fbf296fcd12ea429eae7c7a9
sumankr0087 <suman.kumar@essentia.dev>
2024-10-08 18:41:05 +0530

    shipment journey map

---
+++ shipment.js
@@ -0,0 +1,23 @@
+let map = L.map('map').setView([19.0760, 72.8777], 3);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
+}).addTo(map);
+
+let startCoordinates = [19.0760, 72.8777]; 
+let currentCoordinates = [-36.802237, 12.913736]; 
+let endCoordinates = [40.7128, -74.0060];
+
+
+let startMarker = L.marker(startCoordinates).addTo(map).bindPopup("Start: Mumbai").openPopup();
+let currentMarker = L.marker(currentCoordinates).addTo(map).bindPopup("Current Location").openPopup();
+let endMarker = L.marker(endCoordinates).addTo(map).bindPopup("End: New York").openPopup();
+
+
+let journeyLine = L.polyline([startCoordinates, currentCoordinates, endCoordinates], {
+    color: 'blue',
+    weight: 2,
+    dashArray: '5, 5',
+}).addTo(map);
+
+map.fitBounds(journeyLine.getBounds());
